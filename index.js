7d432ccd73b83b8e0a9c66f5e65000af1a95a6a7
ranjeettiwari09 <ranjeettt88@gmail.com>
2025-10-25 14:22:57 +0530

    vercel add commit

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-const express=require("express")
-const app=express();
-const mongoose=require("mongoose");
-const  {router}=require("./Routes/userAuth")
-const routerNews=require("./Routes/LatestNews")
-const mailerRouter=require("./Routes/nodemailer")
-const tripRrouter=require("./Routes/Trip_Registration")
-const AnnounceRouter=require("./Routes/Announcement")
-const cors=require("cors")
-const dotenv=require("dotenv")
-const cloudinaryConfig=require("./MiddleWare/cloudnary")
-const bookingRouter=require("./Routes/Booking")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const { router } = require("./Routes/userAuth");
+const routerNews = require("./Routes/LatestNews");
+const mailerRouter = require("./Routes/nodemailer");
+const tripRrouter = require("./Routes/Trip_Registration");
+const AnnounceRouter = require("./Routes/Announcement");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cloudinaryConfig = require("./MiddleWare/cloudnary");
+const bookingRouter = require("./Routes/Booking");
 
+dotenv.config();
 
-dotenv.config()
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 cloudinaryConfig();
-mongoose.connect(process.env.MONGODB_URI)//
-app.get("/",(req,res)=>{
-    res.send("console.log listning")
-    console.log("Listing")
-})
-app.use("/auth",router)
-app.use("/news",routerNews)
-app.use("/mailer",mailerRouter)
-app.use("/trip",tripRrouter)
-app.use("/announce",AnnounceRouter)
-app.use("/booking",bookingRouter)
 
-app.listen(process.env.PORT,(req,res)=>{
-   console.log("server is listening on port 5000")
-   console.log("database connected")
-})
+const connectdb = () => {
+  const MONGO_URI = process.env.MONGODB_URI;
+  if (!MONGO_URI) {
+    console.error("❌ MONGODB_URI is not defined!");
+    process.exit(1);
+  }
+  mongoose.connect(process.env.MONGODB_URI); //
+};
+connectdb();
+
+app.get("/", (req, res) => {
+  res.send("console.log listning");
+  console.log("Listing");
+});
+app.use("/auth", router);
+app.use("/news", routerNews);
+app.use("/mailer", mailerRouter);
+app.use("/trip", tripRrouter);
+app.use("/announce", AnnounceRouter);
+app.use("/booking", bookingRouter);
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log("server is listening on port 5000");
+  console.log("database connected");
+});
